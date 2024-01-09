a5b31bf64f4fa47245f9853db4cf024c7131ef54
shilat <shilats2001@gmail.com>
2024-01-09 20:46:42 +0200

    sql function

---
+++ server/helpers/putcall.js
@@ -0,0 +1,7 @@
+const { mysqlConnection } = require('./server/sql/sql');
+
+function putreq(data, id) {
+    const script = 'UPDATE recipes set recipe_rating = (?) where recipe_id = (?)';
+    const values = [data, id];
+    return connection.query(script, values);
+};
